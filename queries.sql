0f00bd1d3e948909d96d4f9c1869d1593a3a5fe9
John Chong <johnchong.99.17@gmail.com>
2023-04-25 14:16:06 -0500

    finish

--- queries.sql
+++ queries.sql
@@ -1,1 +1,26 @@
--- write your queries here
+-- Join two tables so that every column and record appears regardless of owner_id
+SELECT *
+    FROM owners AS o
+    LEFT JOIN vehicles as v
+        ON o.id = v.owner_id;
+
+
+-- Count number of cars for each owner who has a vehicles; display first_name and count
+-- first_name is ASC order
+SELECT first_name, count(*)
+    FROM owners AS o
+    JOIN vehicles AS v
+        ON o.id = v.owner_id
+    GROUP BY o.id
+    ORDER BY first_name;
+
+
+--Count number of cars for each owner and display avg price and count
+--only show where avg price > 10000 and count > 1
+SELECT first_name, ROUND(AVG(price),0) AS average_price, count(*)
+    FROM owners
+    JOIN vehicles
+        ON owners.id = vehicles.owner_id
+    GROUP BY owners.id
+    HAVING AVG(price) > 10000 AND count(*) > 1
+    ORDER BY first_name DESC;
